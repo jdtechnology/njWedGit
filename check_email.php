4511a11e29fa12bdb061f2ed2d538bafc8bcef4a
jd <jackfisher2012@gmail.com>
2017-11-14 16:56:23 +0000

    Update check_email.php to prep statements

--- check_email.php
+++ check_email.php
@@ -3,20 +3,25 @@ require("config.php");
 // Create connection
 $conn = new mysqli(hostname, username, password, dbname);
 $isTaken = 0;
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$eStmt = $conn->prepare("SELECT * FROM guests WHERE email = ? LIMIT 1");
+$eStmt->bind_param("s", $email);
+
 if(isset($_GET['email'])){
 	$email = $conn->real_escape_string($_GET['email']);
-	$sql = "SELECT * FROM guests WHERE email = '$email'";
-	if ($result = $conn->query($sql)) {
+	if ($eStmt->execute()) {
 		//echo "Query successful";
+		$eStmt->store_result();
+		$isTaken = $eStmt->num_rows;
 	} else {
 		//echo "Error: " . $sql . "<br>" . $conn->error;
 	}
-	$isTaken = $result->num_rows;
 }
-$result->close();
+$eStmt->close();
 $conn->close();
 echo $isTaken;
 ?>
