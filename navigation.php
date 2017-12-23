d6f8abae0e82e3e9bc566c335e60d921168e06ff
jdtechnology <jackfisher2012@gmail.com>
2017-12-23 21:05:21 +0000

    General additions

--- navigation.php
+++ navigation.php
@@ -3,18 +3,18 @@
 <div class="container" id="head">
 	<table id="contextmen">
 		<tr>
-			<th><a class="ajax-nav" href="index.php">Home</a></th>
+			<th><a class="ajax-nav" href="index">Home</a></th>
 			<th>
 				<!--<div class="dropdown">-->
-				<a class="dropme ajax-nav" id="dropdown" href="location.php">Location</a>
+				<a class="dropme ajax-nav" id="dropdown" href="location">Location</a>
 					  <div id="myDropdown" class="dropdown-content">
-						<a class="ajax-nav" href="day.php">Daytime</a>
-						<a class="ajax-nav" href="night.php">Nighttime</a>
+						<a class="ajax-nav" href="day">Daytime</a>
+						<a class="ajax-nav" href="night">Nighttime</a>
 					  </div>
 					<!--</div> -->
 			</th>
-			<th><a class="ajax-nav" href="rsvp.php">RSVP</a></th>
-			<th><a class="ajax-nav" href="contact.php">Contact Us</a></th>
+			<th><a class="ajax-nav" href="rsvp">RSVP</a></th>
+			<th><a class="ajax-nav" href="contact">Contact Us</a></th>
 			<th>Hotels</th>
 		</tr>
 	</table>
---
+++ z.php
@@ -0,0 +1,16 @@
+<?php
+/**Constants
+**
+**
+**
+*Update security features here as required
+*CHMOD file so only accessable by server
+**
+**
+**
+**/
+const hostname = "localhost";
+const username = "jdroot";
+const password = "kG@8x$?yS-g;CtBY";
+const dbname = "guest_t1";
+?>
